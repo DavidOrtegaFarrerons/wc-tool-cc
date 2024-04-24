578b2a4d5cb9dbaa392a11528273dce53a10558e
David Ortega <davidortegafarrerons@gmail.com>
2024-04-25 00:38:14 +0200

    Refactor: modified the no_arguments file to better explain the different cli options, what they do and how to run them

--- src/error/no_arguments.py
+++ src/error/no_arguments.py
@@ -1,3 +1,7 @@
 def no_arguments():
     print("You have not sent the correct amount of arguments!")
-    print("To run this command, please use \"python -m src -c file.extension\"")
+    print("The following arguments are available to use:")
+    print("-"*40)
+    print("-c file.extension | Will count the total bytes of the given file")
+    print("-l file.extension | Will count the total lines of the given file")
+    print("To run this command, please use \"python -m src -[c|l] file.extension\"")
