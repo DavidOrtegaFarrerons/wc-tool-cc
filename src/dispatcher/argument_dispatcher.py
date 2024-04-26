3e53507344900ec2fa6efdaa6ced1277f44bc86f
David Ortega <davidortegafarrerons@gmail.com>
2024-04-26 21:50:01 +0200

    refactor: Change keys of map to constants to make it easier to understand. Modify comment.

--- src/dispatcher/argument_dispatcher.py
+++ src/dispatcher/argument_dispatcher.py
@@ -3,17 +3,22 @@ from ..service.line_count_service import count_lines
 from ..service.word_count_service import count_words
 from ..service.char_count_service import count_chars
 
+ARG_COUNT_BYTES = "-c"
+ARG_COUNT_LINES = "-l"
+ARG_COUNT_WORDS = "-w"
+ARG_COUNT_CHARS = "-m"
+ARG_COUNT_LINES_WORDS_BYTES = ""
 
 def argument_dispatch(argument, file_path):
     actions = {
-        "-c": count_bytes,
-        "-l": count_lines,
-        "-w": count_words,
-        "-m": count_chars,
-        "": lambda x: f"{count_lines(x)} {count_words(x)} {count_bytes(x)}"
+        ARG_COUNT_BYTES: count_bytes,
+        ARG_COUNT_LINES: count_lines,
+        ARG_COUNT_WORDS: count_words,
+        ARG_COUNT_CHARS: count_chars,
+        ARG_COUNT_LINES_WORDS_BYTES: lambda x: f"{count_lines(x)} {count_words(x)} {count_bytes(x)}"
     }
 
-    # Check if the provided argument has a corresponding function
+    # Check if the provided argument has a corresponding function or raise exception
     if argument in actions:
         result = actions[argument](file_path)
         print(f"{result} {file_path}")
